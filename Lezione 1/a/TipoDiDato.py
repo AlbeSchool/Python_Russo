49f1c03abba2d2739ed4d9c0b968df5222d83a95
AlbeSchool <alberto.russo@edu.immaginazioneelavoro.it>
2025-03-19 09:36:43 +0100

    Add files via upload

---
+++ Lezione 1/a/TipoDiDato.py
@@ -0,0 +1,23 @@
+# esistono i seguente tipi di dati: String, Number (Integer, Floats), Boolean
+
+print("Questo è un esempio di stringa")
+print("2") #anche questa è una stringa
+print(2) #questo è un integer
+print(2.0) #questo è un float
+print(True) #questo è un boolean
+print(False) #questo è un boolean
+print(1<2) #questo è un boolean
+
+#es
+print("Voglio fare un calcolo semplice")
+
+print(2+2)
+print(3-1)
+print(2*2)
+print(4/2)
+
+# Modulo %, restituisce il resto della divisione
+
+print(5%2) #5 / 2 = 2 resto 1
+
+print(14 % 3) #14 / 3 = 4 resto 2
---
+++ Lezione 1/a/helloWorld.py
@@ -0,0 +1,18 @@
+saldo = 1000
+
+print('ciaone', saldo)
+
+prelievo = 200
+
+saldo = saldo - prelievo
+
+print('hai prelevato €', prelievo, 'e il tuo saldo è €', saldo)
+
+
+depostito = 1400
+
+
+saldo += depostito
+
+
+print('hai depositato €', depostito, 'e il tuo saldo è €', saldo)
